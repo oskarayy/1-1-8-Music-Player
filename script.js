166ca1361e4f5237df5e8f6d7251448f739d95ee
Oskar Czerw <oskar@o2.pl>
2023-12-12 09:02:01 +0100

    two-more-songs

--- script.js
+++ script.js
@@ -1,8 +1,40 @@
+const songs = [
+  {
+    id: 'song-0',
+    author: 'Quebonafide x FORXST',
+    title: 'Madagaskar',
+    img: '/assets/thumb1.png',
+    audio: '/assets/quebo-madagaskar.mp3'
+  },
+  {
+    id: 'song-1',
+    author: 'Gentrammel',
+    title: 'Out of my mind',
+    img: '/assets/mymind.jpg',
+    audio: '/assets/mymind-audio.mp3'
+  },
+  {
+    id: 'song-2',
+    author: 'November',
+    title: 'Mask Off Freestyle',
+    img: '/assets/maskoff.jpg',
+    audio: '/assets/MaskFreestyle.mp3'
+  }
+];
+
 const progress = document.getElementById('progress');
 const song = document.getElementById('song');
 
 const ctrlIcon = document.getElementById('ctrlIcon');
 const ctrlIconBox = document.getElementById('ctrlIconBox');
+
+const backBtn = document.getElementById('backBtn');
+const nextBtn = document.getElementById('nextBtn');
+
+const songSrc = document.querySelector('#song source');
+const title = document.querySelector('.info h1');
+const author = document.querySelector('.info p');
+const thumb = document.getElementById('thumbnail');
 
 song.onloadedmetadata = () => {
   progress.max = song.duration;
@@ -19,7 +51,41 @@ ctrlIconBox.onclick = () => {
   }
 };
 
-if (song.play()) {
+const playSong = (type) => {
+  const activeSongId = songs.findIndex(
+    (item) => item.title === title.innerText
+  );
+  let songToPush;
+
+  switch (type) {
+    case 'next':
+      songToPush =
+        songs[activeSongId + 1 <= songs.length - 1 ? activeSongId + 1 : 0];
+      break;
+    case 'prev':
+      songToPush =
+        songs[activeSongId - 1 > -1 ? activeSongId - 1 : songs.length - 1];
+      break;
+    default:
+      console.log('Switch Error: Wrong parameter.');
+      break;
+  }
+
+  songSrc.src = songToPush.audio;
+  thumb.src = songToPush.img;
+  title.innerHTML = songToPush.title;
+  author.innerHTML = songToPush.author;
+
+  song.load();
+  song.play();
+  ctrlIcon.className = 'fa-solid fa-pause';
+};
+
+song.onended = playSong.bind(null, 'next');
+backBtn.onclick = playSong.bind(null, 'prev');
+nextBtn.onclick = playSong.bind(null, 'next');
+
+if (song.play) {
   setInterval(() => {
     progress.value = song.currentTime;
   }, 500);
